9592a3131bd3b4ea7954419c0d50dfb61eb2ea91
Sharminjahan777 <jahansharmin096@gmail.com>
2018-12-29 20:50:42 -0500

    math problems

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -1,11 +1,33 @@
 package math.problems;
 
+import java.util.ArrayList;
+
+
 public class Fibonacci {
     public static void main(String[] args) {
          /*
           Write 40 Fibonacci numbers with java.
          */
-
-
+        int fab[] = new int[40];
+        System.out.println(arrayFibo(fab));
     }
+
+    public static ArrayList<Integer> arrayFibo(int fab[]) {
+
+        ArrayList<Integer> ar = new ArrayList<>();
+
+        for (int m = 0; m < fab.length; m++) {
+            if (m < 2) {
+                fab[m] = m;
+                ar.add(fab[m]);
+
+            } else {
+                fab[m] = fab[m - 2] + fab[m - 1];
+                ar.add(fab[m]);
+            }
+        }
+
+        return ar;
+    }
+
 }
--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -3,15 +3,26 @@ package math.problems;
 public class MakePyramid {
     public static void main(String[] args) {
 
+        /*   Implement a large Pyramid of stars in the screen with java.
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
+         */
 
-          for (int i=0; i<21; i++){
-              for (int j=0; j<27-i; j++) {
-                  System.out.print(" ");
-              }
-              for (int k=0; k<=i; k++) {
-                  System.out.print(" *");
-              }
-              System.out.println();
-          }
+        int level = 35;
+        for(int i=0; i<level; i++) {
+            for (int j = 0; j < level - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-  }
+
+    }
+
+}
